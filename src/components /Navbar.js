378c2a060f195f32a1aa9d43554abe0cfa67f697
Ali ibrahim <ali.ibrahim.jasim@gmail.com>
2023-02-17 15:11:01 +0300

    The projects now rendering properly inside the projects display

--- src/components /Navbar.js
+++ src/components /Navbar.js
@@ -25,7 +25,6 @@ function Navbar() {
         <Link to="/">Home</Link>
         <Link to="/projects">Projects</Link>
         <Link to="/experience">Experience</Link>
-        <Link to=""></Link>
       </div>
     </div>
   );
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,20 +1,25 @@
-import React from 'react'
-import ProjectItem from '../components /ProjectItem'
-import { projectList } from '../helpers/ProjectList'
+import React from "react";
+import ProjectItem from "../components /ProjectItem";
+import { projectList } from "../helpers/ProjectList";
 
-import '../styles/Projects.css'
+import "../styles/Projects.css";
 
 export default function Projects() {
   return (
-    <div className='projects'>
-     <h1>My personal project</h1>
-     <div className='projectList'>
-       {
-        projectList.map((project) => {
-        return   <ProjectItem key={project.name} name={project.name} image={project.image}/>
-        })
-       }
-     </div>
+    <div className="projects">
+      <h1>My personal project</h1>
+      <div className="projectList">
+        {projectList.map((project, idx) => {
+          return (
+            <ProjectItem
+              id={idx}
+            
+              name={project.name}
+              image={project.image}
+            />
+          );
+        })}
+      </div>
     </div>
-  )
+  );
 }
